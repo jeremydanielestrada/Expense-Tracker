006c7bd4aa3ed1d28ce93023c24717b282d2614b
Jdestrada <jeremydanielestrada2001@gmail.com>
2025-05-26 06:47:11 +0800

    fix: env variables

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -4,6 +4,8 @@ import { useAuthUserStore } from '@/stores/authStore'
 import { onMounted } from 'vue'
 import { useDisplay } from 'vuetify'
 
+const emits = defineEmits(['isDrawervisible'])
+
 //Use Pinia Store
 const authStore = useAuthUserStore()
 
@@ -29,3 +31,46 @@ onMounted(() => {
   getLoggedStatus()
 })
 </script>
+
+<template>
+  <v-responsive>
+    <v-app :theme="theme">
+      <v-app-bar
+        class="px-3"
+        :color="theme === 'light' ? 'light-green-lighten-3' : 'light-green-darken-3'"
+        border
+      >
+        <v-app-bar-title>
+          <v-img src="/images/logo.png" width="60"></v-img>
+        </v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          class="me-2"
+          :icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          variant="plain"
+          slim
+          @click="onToggleTheme"
+        ></v-btn>
+
+        <ProfileHeader v-if="isLoggedIn"></ProfileHeader>
+      </v-app-bar>
+
+      <v-main>
+        <slot name="content"></slot>
+      </v-main>
+
+      <v-footer
+        class="font-weight-bold"
+        :color="theme === 'light' ? 'light-green-lighten-3' : 'light-green-darken-3'"
+        border
+        app
+      >
+        <div :class="mobile ? 'w-100 text-center' : ''">
+          Copyright © 2025 - Petagram | All Rights Reserved
+        </div>
+      </v-footer>
+    </v-app>
+  </v-responsive>
+</template>
