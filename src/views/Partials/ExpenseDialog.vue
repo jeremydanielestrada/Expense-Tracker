068df29c644e1fa880967dd5243ec8dde2584291
Jeremy Daniel Estrada <jeremydanielestrada2001@gmail.com>
2025-05-26 20:38:02 +0800

    feat: added modal

--- src/views/Partials/ExpenseDialog.vue
+++ src/views/Partials/ExpenseDialog.vue
@@ -13,12 +13,12 @@ const close = () => emits('update:modelValue', false)
     :model-value="props.modelValue"
     @update:modelValue="emits('update:modelValue', $event)"
   >
-    <v-card>
+    <v-card class="pa-3">
       <v-card-title>Add Expense</v-card-title>
       <v-form>
-        <v-text-field label="Label"></v-text-field>
-        <v-text-field label="Label"></v-text-field>
-        <v-btn> Add </v-btn>
+        <v-text-field label="What kind of expense?"></v-text-field>
+        <v-text-field type="number" label="Amount"></v-text-field>
+        <v-btn block color="cyan-darken-3"> Add </v-btn>
       </v-form>
       <v-card-actions>
         <v-btn @click="close">Close</v-btn>
